71d12cc285e03b9c763d2d2ecb6cbbd9c4f0e98a
RealAnalysis <detective_boys@hotmail.com>
2019-07-13 13:56:00 +0800

    change icon for hidden_status

--- app/helpers/admin/cities_helper.rb
+++ app/helpers/admin/cities_helper.rb
@@ -1,9 +1,2 @@
 module Admin::CitiesHelper
-  def render_city_status(city)
-    if city.is_hidden
-      "(Hidden)"
-    else
-      "(Public)"
-    end
-  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,2 @@
 module PostsHelper
-  def render_post_status(city, post)
-    if post.is_hidden
-      "(Hidden)"
-    else
-      "(Public)"
-    end
-  end
 end
