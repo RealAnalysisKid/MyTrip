ea1863a0bf8ae2a76cc26973866942b5efa86cbf
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2021-07-04 23:52:28 +0800

    fix rails c issue with pry

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,5 +12,12 @@ module MyTrip
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Use the IRB console instead of the Pry one
+    console do
+      require 'irb'
+      config.console = IRB
+    end
+    
   end
 end
